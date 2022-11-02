a36d9b0408bdfda75bbe40279cc30ca3cfa28bf9
Dipak Ambhure <dipakambhure2001@gmail.com>
2022-11-02 22:27:51 +0530

    function added

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 
+int add(int a, int b){
+	return a+b;
+}
+
 int main(){
 	int a=5;
 	int b=6;
 	int c=a+b;
 	printf("%d+%d=%d\n",a,b,c);
+	printf("Using Function\n");
+	printf("%d+%d=%d\n",10,45,add(10,45));
+
 	return 0;
 }
